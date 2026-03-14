2e31cd4b1b287b8bf339caf1519faf3dbf5f3a08
julesh17 <107618459+julesh17@users.noreply.github.com>
2026-03-14 19:06:39 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
+// FICHIER : next.config.ts (racine du projet)
 import type { NextConfig } from "next";
- 
-const nextConfig: NextConfig = {};
- 
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'peoplespheres.com',
+      },
+    ],
+  },
+};
+
 export default nextConfig;
- 
